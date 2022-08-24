5b400014ca2cdf98a01d11aad5dbb6f21945e598
Giuseppe De Lucia Lumeno <giupetrucci@libero.it>
2022-08-24 20:44:58 +0200

    First commit

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -217,11 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53987" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52150" + '/');
->>>>>>> 28b9ae8303e9d288bbc0d401d970e8b3c4a831aa
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57291" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
